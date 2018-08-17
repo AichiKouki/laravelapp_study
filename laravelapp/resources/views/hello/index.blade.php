b8e9f6abe9c866868d79e185f1685a36b109527b
愛知巧規 <prosenyou123@gmail.com>
2018-08-17 18:30:04 +0900

    フィールドごとにエラー表示する処理を作ってみた

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -10,21 +10,24 @@
 
 @section('content')
 <p>{{$msg}}</p>
-//$errorはバリデーションで発生したエラーメッセージをまとめて管理するオブジェクト
-@if(count($error)>0)//$errorはバリデーションの機能によって組み込まれている
-<div>
-	<ul>
-		@foreach($error->all() as $error)//エラーメッセージをまとめて取り出す
-			<li>{{$error}}</li>
-		@endforeach
-	</ul>
-</div>
+{{--$errorはバリデーションで発生したエラーメッセージをまとめて管理するオブジェクト--}}
+@if(count($errors)>0) {{--$errorはバリデーションの機能によって組み込まれている--}}
+<p>入力に問題があります。再入力してください</p>
 @endif
 <table>
 <form action="/hello" method="post">
 	{{csrf_field()}}
+	@if($errors->has('name')) {{--hasメソッドは、エラーがあるかどうかをチェック--}}
+	<tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>{{--firstメソッドは、指定した項目の最初のエラーメッセージを取得--}}
+	@endif
 	<tr><th>name:</th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+		@if($errors->has('mail'))
+	<tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+	@endif
 	<tr><th>mail:</th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>	
+		@if($errors->has('age'))
+	<tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+	@endif
 	<tr><th>age:</th><td><input type="text" name="age" value="{{old('age')}}
 	"></td></tr>
 	<tr><th>name:</th><td><input type="submit" value="send"></td></tr>
