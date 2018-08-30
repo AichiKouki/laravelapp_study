17f7f884d95b48f6dd6a619905d0f13333eb954c
愛知巧規 <prosenyou123@gmail.com>
2018-08-30 16:47:04 +0900

    マイグレーションファイルの生成と実際にコマンド入力して試した

---
+++ laravelapp/database/migrations/2018_08_30_064359_create_people_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *upメソッドはテーブルを生成するための処理を記述する
+     * @return void
+     */
+    public function up()
+    {
+//テーブル作成には、Schemaクラスのcreateメソッドを利用
+//第一引数にテーブル名、第二引数にはテーブルを作成するための処理
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('mail');
+            $table->integer('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *downメソッドはテーブルを削除するための処理を記述しておきます
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
